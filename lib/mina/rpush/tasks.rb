3bdba8ad438f39851f96037a5243965f1e7326c6
Andrey Ilyunin <andrey.ilyunin@jetruby.com>
2018-05-17 14:46:33 +0300

    Replace :environment with :remote_environment

--- lib/mina/rpush/tasks.rb
+++ lib/mina/rpush/tasks.rb
@@ -4,31 +4,31 @@ namespace :rpush do
   set :rpush_command, -> { "cd #{fetch(:current_path)} && #{fetch(:bundle_prefix)} rpush" }
 
   desc 'Start rpush'
-  task start: :environment do
+  task start: :remote_environment do
     command %[echo '-----> Starting rpush']
     command "#{fetch(:rpush_command)} start -e #{fetch(:rails_env)}"
   end
 
   desc 'Stop rpush'
-  task stop: :environment do
+  task stop: :remote_environment do
     command %[echo '-----> Stopping rpush']
     command "#{fetch(:rpush_command)} stop -e #{fetch(:rails_env)}"
   end
 
   desc 'Restart rpush'
-  task restart: :environment do
+  task restart: :remote_environment do
     command %[echo '-----> Restarting rpush']
     command "#{fetch(:rpush_command)} stop -e #{fetch(:rails_env)}; #{fetch(:rpush_command)} start -e #{fetch(:rails_env)}"
   end
 
   desc 'Push pending notifications'
-  task push: :environment do
+  task push: :remote_environment do
     command %[echo '-----> Pushing pending notifications']
     command "#{fetch(:rpush_command)} push -e #{fetch(:rails_env)}"
   end
 
   desc 'Check rpush status'
-  task status: :environment do
+  task status: :remote_environment do
     command %[echo '-----> Checking rpush status']
     command "#{fetch(:rpush_command)} status -e #{fetch(:rails_env)}"
   end
